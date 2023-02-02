c8db294a88da9c0075cb3685207d139c54fb2859
root <hassaa73@mcmaster.ca>
2023-02-02 13:54:31 -0500

    Fix Shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_LINE 80
 
@@ -16,17 +17,24 @@ int main(void)  {
         size_t inputLen;
 
         inputBuffer = (char *)malloc(bufsize * sizeof(char));
+        char *Arguments[3];// = malloc(sizeof(char*)*2);
         inputLen = getline(&inputBuffer,&bufsize,stdin);
 
         //Split input buffer into program name, and arguments
-        char *programName = strtok(inputBuffer, " ");
-        char *programArguments = strtok(NULL, "\0");
+        char *programName = strtok(inputBuffer, " \t\r\n\f\v");
+        char *programArguments = strtok(NULL, "\n");
+
+        Arguments[0] = programName;
+        Arguments[1] = programArguments;
+        Arguments[2] = NULL;
 
         //remove trailing newline characters
         //programName[strcspn(programName, "\n")] = 0; 
         //programArguments[strcspn(programArguments, "\n")] = 0;
-        printf("Program Name = %s\n", programName);
-        printf("Program Arguments = %s\n", programArguments);
+        //printf("Program Name = %s\n", programName);
+        //printf("Program Arguments = %s\n", Arguments[1]);
+        //printf("Program Arguments2 = %s\n", Arguments[2]);
+
 
         pid_t pid = fork();
         int execStatus;
@@ -41,7 +49,8 @@ int main(void)  {
             /* child process */
             printf("Child Process: \n");
             // argv[1] is the command, and &argv[1] is the address of rest
-            execStatus = execvp(programName, programArguments); 
+            execStatus = execvp(Arguments[0], &Arguments); 
+            printf("Exec Status Child Process: %d\n", execStatus);
             //return execStatus;
             exit(0);
         }
